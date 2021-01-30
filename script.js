eac01a7e00f69934f0902fdecc98d8d84abe36c1
adriand1az <adrian111699@gmail.com>
2021-01-30 11:02:30 -0600

    minor fix

--- script.js
+++ script.js
@@ -3,13 +3,15 @@ const icons = document.querySelectorAll('.fab')
 console.log(icons)
 
 for (const icon of icons) {
+
     icon.addEventListener('mouseover', function (event) {
         icon.classList.add('fa-spin')
-        //...
     })
+
     icon.addEventListener('mouseleave', function (event) {
         icon.classList.remove('fa-spin')
     })
+
 }
 
 
